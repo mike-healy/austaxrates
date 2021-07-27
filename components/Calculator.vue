ae434bd46679f30cc698fd6083970ae609e7ad4f
Mike Healy <design@mikehealy.com.au>
2021-07-27 17:08:49 +1000

    Fix min-h-screen and copy and design tweaks for better clarity.

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -29,34 +29,37 @@
         </p>
       </div>
 
-      <section class="p-6 py-2 border border-solid border-green-400">
+      <section class="p-6 py-2 border border-solid border-yellow-400">
 
-        <div class="py-2 text-xl">
-          <span class="inline-block w-36">Nett</span>
-          <output class="py-1 px-2 bg-gradient-to-tr from-green-500 to-green-200 text-gray-900">${{ round(nett()).toLocaleString() }}</output>
+        <div class="py-4 text-xl">
+          <span class="inline-block text-yellow-200 w-36">Nett</span>
+          <output class="py-2 px-4 bg-gradient-to-tr from-yellow-400 to-white text-gray-900">${{ round(nett()).toLocaleString() }}</output>
         </div>
         
         <div class="py-2">
-          <p class="mb-1">
-            <span class="inline-block w-36 pl-4">/week</span>
-            <output class="py-1 px-2 bg-gradient-to-tr from-green-300 to-green-200 text-gray-900">{{ round(nett()/52).toLocaleString() }}</output>
+          <p class="mb-2">
+            <span class="inline-block text-right w-36 px-4 text-yellow-200">/ week</span>
+            <output class="py-1 px-2 bg-gradient-to-tr from-yellow-300 to-yellow-200 text-gray-900">${{ round(nett()/52).toLocaleString() }}</output>
           </p>
-          <p class="mb-1">
-            <span class="inline-block w-36 pl-4">/fortnight</span>
-            <output class="py-1 px-2 bg-gradient-to-tr from-green-300 to-green-200 text-gray-900">{{ round(nett()/26).toLocaleString() }}</output>
+          <p class="mb-2">
+            <span class="inline-block text-right w-36 px-4 text-yellow-300">/ fortnight</span>
+            <output class="py-1 px-2 bg-gradient-to-tr from-yellow-400 to-yellow-300 text-gray-900">${{ round(nett()/26).toLocaleString() }}</output>
           </p>
-          <p class="mb-1">
-            <span class="inline-block w-36 pl-4">/month</span>
-            <output class="py-1 px-2 bg-gradient-to-tr from-green-300 to-green-200 text-gray-900">{{ round(nett()/12).toLocaleString() }}</output>
+          <p class="mb-2">
+            <span class="inline-block text-right w-36 px-4 text-yellow-400">/ month</span>
+            <output class="py-1 px-2 bg-gradient-to-tr from-yellow-500 to-yellow-400 text-gray-900">${{ round(nett()/12).toLocaleString() }}</output>
           </p>
         </div>
 
       </section>
 
       <!-- Result rates -->
-      <section class="p-4 mt-6 border border-solid border-gray-700 text-gray-300">
-        <p class="mb-2">{{ round(( tax()+medicare() )/income * 100) }}% overall tax &amp; medicare combined rate.</p>
-        <p>{{ currentIncomeBracket.rate*100 }}% top marginal rate (excluding Medicare levy of {{ medicareData.rate*100 }}%)</p>
+      <section class="p-4 mt-6 border border-solid border-gray-700 text-gray-300 shadow:md">
+        <p class="mb-2"><span class="text-green-300">{{ round(( tax()+medicare() )/income * 100) }}% <span class="underline">overall</span></span> tax &amp; medicare levy combined rate.</p>
+        <p>
+          <span class="text-green-300">{{ currentIncomeBracket.rate*100 }}% top marginal rate</span>
+          <span v-show="medicare() > 0">(excluding Medicare levy of {{ medicareData.rate*100 }}%)</span>.
+        </p>
       </section>
 
     </form>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,17 @@
 <template>
-  <div class="h-screen p-12 bg-gray-800 text-white">
-    <div class="md:grid grid-flow-col gap-12">
-      <main class="order-2">
+  <div class="min-h-screen p-12 bg-gray-800 text-white">
+    <div class="md:grid grid-flow-col gap-24">
+      <main>
         <Calculator />
-        
       </main>
-      <aside class="order-1">
+
+      <aside class="pt-12">
         <div class="max-w-md p-4 bg-gray-900 rounded-md shadow-md">
           <h2 class="text-green-300 mb-2 text-xl">Please Note:</h2>
-          <ul class="pl-8 text-gray-300">
-            <li class="my-4 border-b border-solid border-gray-700">Calculation based on simple income tax rates from ATO</li>
-            <li class="my-4 border-b border-solid border-gray-700">Does not account for offsets, deductions, family tax benefits or other complications</li>
-            <li class="my-4 border-b border-solid border-gray-700">Medicare levy is estimated at full rate when income is over the threshold. Low income earners may get a discount, reducing the levy</li>
-          </ul>
-
-          <h3 class="mt-12 text-green-300 mb-2">To Do</h3>
-          <ul>
-            <li class="mb-2">
-              Select tax year
-            </li>
-            <li class="mb-2">
-              Multiple calculator instances to compare with
-            </li>
+          <ul class="text-gray-300">
+            <li class="py-2 my-2 border-b border-solid border-gray-700">Calculation based on simple income tax rates from ATO</li>
+            <li class="py-2 my-2 border-b border-solid border-gray-700">Does not account for offsets, deductions, family tax benefits or other complications</li>
+            <li class="py-2 my-2 border-b border-solid border-gray-700">Medicare levy is estimated at full rate when income is over the threshold. Low income earners may get a discount, reducing the levy</li>
           </ul>
         </div>
       </aside>
